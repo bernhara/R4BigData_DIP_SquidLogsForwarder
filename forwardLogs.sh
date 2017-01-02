d691a238eda8005695654b11ec9869ee2bdf1c75
bernhara <raphael.bernhard@orange.com>
2017-01-02 14:21:13 +0100

    remove out redirection in forwarder script

--- forwardLogs.sh
+++ forwardLogs.sh
@@ -3,17 +3,6 @@
 HERE=`dirname $0`
 CMD=`basename $0`
 
-: ${stdout_log_file:="${HERE}/${CMD}.stdout.log"}
-: ${stderr_log_file:="${HERE}/${CMD}.stderr.log"}
-
-
-: ${redirect_output:=true}
-
-if ${redirect_output}
-then
-    exec 1>"${stdout_log_file}" 2>"${stderr_log_file}"
-fi
-
 if [ -r "${HERE}/${CMD}-config" ]
 then
     . "${HERE}/${CMD}-config"
